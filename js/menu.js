b46a0baef8df8ba7f4b214b84eb357a54c2692d6
yuro4kaw <tahkict.yup4ik@gmail.com>
2023-03-19 20:57:19 +0200

    Final Mobile Menu

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,8 @@
 const menuBtnRef = document.querySelector("[data-menu-button]");
 const mobileMenuRef = document.querySelector("[data-menu]");
+const logo = document.querySelector("[data-logo]");
+const body = document.querySelector("[data-body]");
+const html = document.querySelector("[data-html]");
 
 menuBtnRef.addEventListener("click", () => {
     const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
@@ -7,4 +10,7 @@ menuBtnRef.addEventListener("click", () => {
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
+    logo.classList.toggle("is-open");
+    body.classList.toggle("is-open");
+    html.classList.toggle("is-open");
 })
